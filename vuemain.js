27cefa1030d1d8337cb72985e4cbcb6a265f8d03
Tanis Webb <taniswebb@gmail.com>
2018-01-27 02:38:26 +0100

    muh

--- vuemain.js
+++ vuemain.js
@@ -1,6 +1,5 @@
 const shell = require('node-powershell');
 
-
 const vueapp = new Vue({
   el: '#vueapp',
   data: {
@@ -12,23 +11,26 @@ const vueapp = new Vue({
     command: '',
     gpunum: '0',
     gpulist: '',
-    poolname: 'hashrefinery',
+    poolnames: [
+      "zpool",
+      "hashrefinery",
+      "miningpoolhub",
+      "nicehash",
+      "hashpool"
+    ],
+    poolname: '',
     location: 'US',
     donate: '5',
+    gainpct: '3',
     checkedAlgos: [],
     alglolist: '',
-    totalAlgos: [
-      "skunk",
-      "phi",
-      "xevan",
-      "tribus",
-      "skein",
-      "bitcore",
-      "x17",
-      "Nist5",
-      "Lyra2RE2",
-      "neoscrypt"
-    ]
+    pools: {
+      hashpool: [ "xevan", "hsr", "phi", "tribus", "c11", "lbry", "skein", "sib", "bitcore", "x17", "Nist5", "myr-gro", "Lyra2RE2", "neoscrypt", "blake2s", "skunk" ],
+      hashrefinery: [ "skunk", "phi", "xevan", "tribus", "skein", "bitcore", "x17", "Nist5", "Lyra2RE2", "neoscrypt" ],
+      miningpoolhub: [ "cryptonight", "keccak", "lyra2z", "skein", "equihash", "groestl", "myr-gro", "Lyra2RE2", "neoscrypt" ],
+      zpool: [ "poly", "hsr", "keccak", "xevan", "veltor", "skunk", "tribus", "c11", "x11evo", "lbry", "phi", "skein", "equihash", "groestl", "timetravel", "sib", "bitcore", "x17", "blakecoin", "Nist5", "myr-gro", "Lyra2RE2", "neoscrypt", "blake2s" ],
+      nicehash: [ "cryptonight", "keccak", "skunk", "lbry", "equihash", "Nist5", "Lyra2RE2", "neoscrypt", "blake2s" ]
+    }
   },
   methods: {
     startMining() {
@@ -42,13 +44,20 @@ const vueapp = new Vue({
       // console.log(this.Algos)
       ps.addCommand(this.Algos) // -SelGPUDSTM '0' -SelGPUCC '0' -Currency USD -Passwordcurrency DGB -interval 30 -Wallet D5STb4D1RDa1qXv4x2DX9YgAfPub9rRVrQ -Location US -ActiveMinerGainPct 3 -PoolName hashrefinery -WorkerName ID=NemosMiner-v2.4.1 -Type nvidia -Algorithm skunk,phi,tribus,skein,bitcore,Nist5,Lyra2RE2,neoscrypt,yescrypt -Donate 0")
       ps.invoke()
-      .then(output => {
-        console.log(output);
-      })
-      .catch(err => {
-        console.log(err);
-        ps.dispose();
-      });
+        .then(output => {
+          console.log(output);
+        })
+        .catch(err => {
+          console.log(err);
+          ps.dispose();
+        });
+    },
+    downloadBat(name, type, pool) {
+      this.poolname = pool;
+      let a = document.getElementById("downloadClass");
+      let file = new Blob([this.Algos], {type: type});
+      a.href = URL.createObjectURL(file);
+      a.download = name;
     }
   },
   computed: {
@@ -69,9 +78,18 @@ const vueapp = new Vue({
     Algos() {
       this.algolist = this.checkedAlgos.join()
 
+      String.prototype.format = function() {
+        var newStr = this
+        i = 0
+        while (/%s/.test(newStr)) {
+          newStr = newStr.replace("%s", arguments[i++])
+        }
+        return newStr
+      }
+
       this.command = [
-        // "powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle maximized -command \"&.\\NemosMiner-v2.4.1.ps1",
-        "&.\\scripts\\NemosMiner-v2.4.1.ps1",
+        "powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle maximized -command",
+        "\"" + __dirname + "\\scripts\\NemosMiner-v2.4.2.ps1",
         "-SelGPUDSTM \'" + this.gpuNumbers.gpus + "'",
         "-SelGPUCC \'" + this.gpuNumbers.gpuc + "'",
         "-Currency USD",
@@ -79,12 +97,12 @@ const vueapp = new Vue({
         "-Interval 30",
         "-Wallet " + this.walletadress,
         "-Location " + this.location,
-        "-ActiveMinerGainPct 3",
-        "-PoolName " + this.poolname,
+        "-ActiveMinerGainPct " + this.gainpct,
+        "-PoolName " + this.poolname.toLowerCase(),
         "-WorkerName " + this.workername,
         "-Type nvidia",
         "-Algorithm " + this.algolist,
-        "-Donate " + this.donate
+        "-Donate " + this.donate + "\""
       ]
 
       this.command = this.command.join(' ')
@@ -99,4 +117,4 @@ const vueapp = new Vue({
 //TODO: Execute command
 
 /*
-*/
+ */
